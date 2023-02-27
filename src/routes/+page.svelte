36b5ce0974d54c12adf3bc54e81f990750054bb5
Scott Meadows <Scottmeadows04@gmail.com>
2023-02-27 10:06:02 -0500

    init: initail commit
    separated this directory from the portfolio backend, where it used to be stored as ./client

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { admin } from '../stores';
+  import Header from '../Header.svelte';
+  import About from '../About.svelte';
+  import Portfolio from '../Portfolio.svelte';
+  import Contact from '../Contact.svelte';
+  import ProjectForm from '../ProjectForm.svelte';
+	import Options from '../Options.svelte';
+	import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const response = await fetch('/api/me');
+
+    if (response.ok) {
+      const loginData = await response.json();
+      admin.login(loginData);
+    }
+  });
+</script>
+
+<main>
+  <section id="landing">
+    <Header />
+    <About />
+  </section>
+  <Portfolio />
+  <Contact />
+  {#if $admin.id}
+  <p>Logged in as {$admin.email}</p>
+  <ProjectForm />
+  {/if}
+  <Options />
+</main>
+
+<style lang="sass">
+  section
+    &#landing
+      min-height: 100vh
+</style>

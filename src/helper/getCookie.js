f6dd5834fb4fc238a0f6454d9a9ce3b862fab53b
Scott Meadows <Scottmeadows04@gmail.com>
2023-03-03 16:14:33 -0500

    fix: fix getCookie no method error
    added default return in case the cookie does not exist yet.

--- src/helper/getCookie.js
+++ src/helper/getCookie.js
@@ -3,5 +3,9 @@ export const getCookie = (cname) => {
   const cookies = document.cookie.split(';');
 
   const cookie = cookies.find(c => c.trim().startsWith(name));
-  return cookie.slice(name.length);
+
+  if (cookie)
+    return cookie.slice(name.length);
+  else
+    return '';
 }
